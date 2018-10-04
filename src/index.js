61d85cbe3e157d05bb05c63877b0fbceecebcb41
lVeliarl <dimalitx@gmail.com>
2018-10-04 04:31:35 +0300

    advanced-zeros/imp: partial implementation of getZerosCount func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+
+  const factors = [];
+
+  function getPrimeFactors(remainder) {
+    for (let i = 2; i <= remainder; i++) {
+      while ((remainder % i) === 0) {
+        factors.push(i);
+        remainder /= i;
+      }
+    }
+    return factors;
+  }
+
+  const prime = getPrimeFactors(base)[factors.length - 1];
+  let count = 0;
+
+  for (let j = prime; number / j >= 1; j *= prime) {
+    count = count + Math.floor(number / j);
+  }
+  return count;
 }
